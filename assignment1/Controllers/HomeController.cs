39d252f7c233b2c2ea76536f986372f409c8cb49
Anh Trinh <wildmanyt@gmail.com>
2023-02-24 19:39:18 +0700

    update for controllers

--- assignment1/Controllers/HomeController.cs
+++ assignment1/Controllers/HomeController.cs
@@ -159,7 +159,7 @@ namespace assignment1.Controllers
             var cart = HttpContext.Session.GetString("cart");
             if (cart == null)
             {
-                return NotFound();
+                HttpContext.Session.SetString("cart", JsonConvert.SerializeObject(new List<CartItem>()));
             }
             List<CartItem>? dataCart = JsonConvert.DeserializeObject<List<CartItem>>(cart);
             if (dataCart == null)
