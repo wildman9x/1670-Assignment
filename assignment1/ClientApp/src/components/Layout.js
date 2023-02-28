e36c899e2354f353e0105231096bb9c80162d67a
wildman9x <wildmanyt@gmail.com>
2023-02-28 23:23:25 +0700

    add cart to layout

--- assignment1/ClientApp/src/components/Layout.js
+++ assignment1/ClientApp/src/components/Layout.js
@@ -13,6 +13,30 @@ export class Layout extends Component {
                     href="https://fonts.googleapis.com/css?family=Lexend+Exa&amp;display=swap"
                 ></link>
 
+                {/* stickyCart */}
+                <div
+                    className="desktop-cart"
+                    style={{width: "100%",maxWidth: "1900px",position: "fixed",top: "0px",zIndex: "200",}}
+                >
+                    <div className="cart-container"></div>
+                    <div>
+                        <button
+                            className="cart-info btn-custom"
+                            type="button"
+                            title="View cart"
+                            // onClick="location.href = &#39;cart.html&#39;;"
+                        >
+                            <span className="cart-count button">0</span>
+                            <span className="cart-total-amount">
+                                <span className="currency-sign">$</span>
+                                <span>0.00</span>
+                            </span>
+                        </button>
+                    </div>
+                </div>
+                {/* end of stickyCart */}
+
+                {/* Welcome screen */}
                 <div className="wrap">
                     <section className="welcome gradient home-welcome">
                         <div className="welcome-messaging">
@@ -46,9 +70,15 @@ export class Layout extends Component {
                             <img src=""></img>
                         </div>
                     </section>
+                    {/* end of welcome */}
+
+                    {/* StickyCart2 */}
+                    <div className="sticky-cart sticky"></div>
+                    {/* end of StickyCart2 */}
+
                     <div className="main">
-                    <NavMenu />
-                    <Container>{this.props.children}</Container>
+                        <NavMenu />
+                        <Container>{this.props.children}</Container>
                     </div>
                 </div>
             </div>
