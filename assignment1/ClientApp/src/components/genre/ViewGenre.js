df2b6a84178c6d02b40501c161aa6f7dc86fd178
Nghiadao1 <105330547+Nghiadao1@users.noreply.github.com>
2023-03-12 22:35:12 +0700

    update Genre

--- assignment1/ClientApp/src/components/genre/ViewGenre.js
+++ assignment1/ClientApp/src/components/genre/ViewGenre.js
@@ -11,5 +11,41 @@ export const ViewGenre = () => {
     dispatch(fetchGenres());
   }, [dispatch]);
 
- 
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <h1>View Genres</h1>
+        <a href="/genre/create" className="button">Create Genre</a>
+      </div>
+      {genreState.loading ? (
+        <p>Loading...</p>
+      ) : (
+        <table className="table table-striped" aria-labelledby="tabelLabel">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            {genres.map((genre) => (
+              <tr key={genre.id}>
+                <td>{genre.name}</td>
+                <td>{genre.description}</td>
+                <td>
+                  <a href={"/genre/" + genre.id}>Details</a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
 };
